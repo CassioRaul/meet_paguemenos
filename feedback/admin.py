deb61c285a46afabf5ac2164d7b5aab2da0f708c
ICARO <icaro.realiza@gmail.com>
2023-06-15 21:23:36 -0300

    alteracao feedback

--- feedback/admin.py
+++ feedback/admin.py
@@ -3,11 +3,11 @@ from .models import Feedback, DeleteFeedback
 
 class ListFeedback(admin.ModelAdmin):
 
-    list_display = ('feedback_iduser', 'feedback_title', 'feedback_author', 'feedback_receiver', 'feedback_date', 'feedback_desc', 'feedback_evaluate', )
+    list_display = ('feedback_iduser', 'feedback_title', 'feedback_manage', 'feedback_collaborator', 'feedback_date','feedback_quest1',  'feedback_quest2', 'feedback_quest3','feedback_quest4',  'feedback_quest5', 'feedback_quest6','feedback_desc', 'feedback_evaluate')
 
 admin.site.register(Feedback, ListFeedback)
 
 class ListDeleteFeedback(admin.ModelAdmin):
-    list_display = ('delete_feedback_id', 'delete_feedback_date_time_end','delete_feedback_ipadress', 'delete_feedback_browser', 'delete_feedback_iduser', 'delete_feedback_author','delete_feedback_receiver', 'delete_feedback_title', 'delete_feedback_desc', 'delete_feedback_date_time', 'delete_feedback_evaluate',)
+    list_display = ('delete_feedback_id', 'delete_feedback_date_time_end','delete_feedback_ipadress', 'delete_feedback_browser', 'delete_feedback_iduser', 'delete_feedback_manage','delete_feedback_collaborator', 'delete_feedback_title','delete_feedback_quest1', 'delete_feedback_quest2','delete_feedback_quest3', 'delete_feedback_quest4', 'delete_feedback_quest5','delete_feedback_quest6',  'delete_feedback_desc', 'delete_feedback_date_time', 'delete_feedback_evaluate')
 
 admin.site.register(DeleteFeedback, ListDeleteFeedback)
