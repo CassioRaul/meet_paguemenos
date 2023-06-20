66f2522497b989cc68a5a64fc35a9d553fd9cd2e
ICARO <icaro.realiza@gmail.com>
2023-06-20 11:08:20 -0300

    Criacao fbck c e alteracao de campos fdbc g

--- feedback/urls.py
+++ feedback/urls.py
@@ -4,13 +4,22 @@ from rest_framework import routers
 from . import views
 from feedback.api import viewsets as feedbackviewsets
 
-route_feedback = routers.DefaultRouter()
-route_feedback.register(r'', feedbackviewsets.FeedbackViewSet)
+route_feedbackg = routers.DefaultRouter()
+route_feedbackg.register(r'', feedbackviewsets.FeedbackGViewSet)
 
-route_del_feedback = routers.DefaultRouter()
-route_del_feedback.register(r'', feedbackviewsets.DeleteFeedbackViewSet)
+route_del_feedbackg = routers.DefaultRouter()
+route_del_feedbackg.register(r'', feedbackviewsets.DeleteFeedbackGViewSet)
+
+route_feedbackc = routers.DefaultRouter()
+route_feedbackc.register(r'', feedbackviewsets.FeedbackCViewSet)
+
+route_del_feedbackc = routers.DefaultRouter()
+route_del_feedbackc.register(r'', feedbackviewsets.DeleteFeedbackCViewSet)
+
 
 urlpatterns = [
-    path('feedback/', include(route_feedback.urls)),
-    path('delete_feedback/', include(route_del_feedback.urls)),
+    path('feedbackg/', include(route_feedbackg.urls)),
+    path('delete_feedbackg/', include(route_del_feedbackc.urls)),
+    path('feedbackc/', include(route_feedbackc.urls)),
+    path('delete_feedbackc/', include(route_del_feedbackc.urls)),
 ]
